391d5c3eafcbb3ff31e7367c57eb583fe0c88be0
mvksxm <maksimum232@gmail.com>
2025-12-08 22:20:09 -0500

    Added an about section of the website

--- frontend/personal-website/src/components/About/About.tsx
+++ frontend/personal-website/src/components/About/About.tsx
@@ -1,9 +1,71 @@
-import "./About.css"
+import "./About.scss"
+import images from "../../../public/exporter"
+import Skills from "./Skills/Skills";
 
 const About = () => {
+    
+    const skillsObject = {
+        "Languages": [
+            "Python",
+            "Golang",
+            "Terraform",
+            "Bash",
+            "SQL",
+            "C#",
+            "JavaScript",
+            "Java"
+        ],
+        "Tools & Frameworks": [
+            "Docker",
+            "K8S", 
+            "Django",
+            "Flask",
+            "React"
+        ]
+    }
+
+
+    const skillsMap = new Map<string, Array<string>>(
+        Object.entries(skillsObject)
+              .map(([key, value]) => [key, [...value]])
+    )
+                                       
+
     return (
         <div className="container-about">
-            <h1>Hello!</h1>
+            <h1 className="head-about">About <b>Me</b></h1>
+            <div className="row" style={{justifyContent: "center"}}>
+                <div className="col col-general col-centered">
+                    <img className="personal-photo" src={images["caracal.jpg"]}></img>
+                </div>
+                <div className="col col-general">
+                    <b>
+                        <span style={{color: "white", textTransform: "uppercase"}}>Greetings! Thank you for visiting my <b>webpage!</b></span>
+                    </b>
+                    <h3>My name is <b>Maksim Turtsevich.</b></h3>
+                    <p>
+                        I'm a results-driven <b>Software Engineer</b> with the specialization in <b>Cloud</b> and <b>Platform Development</b>.
+                        With 4 years of commercial experience in <b>Cloud Infrastructure</b>  &  <b>Backend Software Development</b> and 
+                        with deep expertise in <b>Google Cloud</b>, <b>Terraform</b>, and <b>System Engineering</b>, 
+                        I specialize in designing and implementing efficient, scalable, and cost-effective cloud solutions tailored to diverse use cases, 
+                        including <b>data pipelines</b>, <b>microservice-based</b> enterprise applications, and <b>CI/CD pipelines</b>.
+                    </p>
+                    <p>
+                        Beyond my <b>Cloud Engineering</b> expertise, I bring robust <b>Software Engineering</b> skills, particularly in <b>Big Data</b> and <b>Backend Development</b>. 
+                        Proficient in <b>Python</b>, <b>Golang</b>, <b>Docker</b>, and <b>Kubernetes</b>, I excel in roles requiring the end-to-end design and implementation of complex solutions.
+                    </p>
+                    <p>
+                        Finally, I also possess a <b>MSc. in Computer Science</b> from the <b>Polish Japanese Academy of the Information Technology</b> located 
+                        in <b>Warsaw, Poland</b>. My final thesis's research topic was related to <b>Cloud</b> and <b>Big Data Engineering</b>.
+                    </p>
+                    <p>
+                        Feel free to reach out and connect with me on LinkedIn! Also, you can definitely drop a message on my Email, in case, if it's a more preferred option!
+                    </p>
+                    <div className="row" style={{marginTop: "1vh"}}>
+                        <Skills skillsMap={skillsMap} ></Skills>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ frontend/personal-website/src/components/About/Skills/Skills.tsx
@@ -0,0 +1,68 @@
+import "./Skills.css"
+import images from "../../../../public/exporter";
+
+// Constants
+const skillsPerRow = 4;
+const skillTypesLimit = 2;
+
+interface SkillsProps {
+    skillsMap: Map<string, Array<string>>;
+}
+
+
+const arrangeSkills = (skills: Array<string>) => {
+    var rowRange: Array<number> = [];
+    
+    for (var rowId = 0; rowId < Math.ceil(skills.length / skillsPerRow); rowId++) {
+        rowRange.push(rowId)
+    }
+
+    return (
+        rowRange.map((colId: number) =>
+            <div className="skills-row">
+                {
+                    skills.slice(colId * skillsPerRow, (colId + 1) * skillsPerRow).map(
+                        skill => <div className="skill">{skill}</div>
+                    )   
+                }
+            </div>
+        )
+    )
+}
+
+const skillsPerKey = (skillsMap: Map<string, Array<string>>) => {
+    var skillsMapKeys = Array.from(skillsMap.keys())
+    return (
+        skillsMapKeys.map((key: string, idx: number) => 
+            idx < skillTypesLimit ?
+                <div className="col">
+                    <div className="skills-box">
+                        <div className="skills-row">
+                            <img className="terminal-pic" src={images["terminal.svg"]}></img>
+                            <h4 className="skills-header">{key}</h4>
+                        </div>
+                        {arrangeSkills(skillsMap.get(key) ?? [])}
+                    </div>
+                </div>
+                : <div></div>
+        )
+    )
+}
+
+const Skills = ({skillsMap}: SkillsProps) => {
+
+    if (skillsMap.keys.length > skillTypesLimit) {
+        throw new Error(`
+            Skills Map provided to the 'Skills' component has more skill 
+            types than the number supported -> ${skillTypesLimit}!`
+        )
+    }
+
+
+    return (
+        skillsPerKey(skillsMap)
+    )
+}
+
+
+export default Skills
