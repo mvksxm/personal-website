7e386bf88464b751fbd8da58d17c483611c91232
mvksxm <maksimum232@gmail.com>
2025-12-08 22:11:44 -0500

    Added initial version of the components for the work section.

---
+++ frontend/personal-website/src/components/Work/Work.tsx
@@ -0,0 +1,25 @@
+
+
+const workExperienceObject = [
+    {
+        "companyName": "",
+        "dateStart": "",
+        "dateEnd": "", 
+        "location": "",
+        "logoUrl": "", 
+        "jobResponsibilities": []
+        
+    }
+
+]
+
+const Work = () => {
+    return (
+        <div className="work-container">
+            <h1>My Work Experience!</h1>
+        </div>
+    )
+}
+
+
+export default Work
