43058b33ef62ab145c50df66d214c122d4312f72
mvksxm <maksimum232@gmail.com>
2025-12-28 23:08:01 -0500

    Connected backend for the Work Experience section and changed a respective test data.

--- frontend/personal-website/src/components/Work/Work.tsx
+++ frontend/personal-website/src/components/Work/Work.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type JSX } from "react"
+import { useEffect, useState } from "react"
 import "./Work.css"
 import axios from "axios";
 import type { StringDivProps, WorkExperienceType } from "../../models";
@@ -23,61 +23,24 @@ const dtf = new Intl.DateTimeFormat("en-US", {
 
 const testData = [
     {
-        "id": "QWJib3R0",
-        "companyName": "Abbott",
-        "position": "Intern DevOps/Data Engineer",
-        "dateStart": "2021-12-01",
-        "dateEnd": "2022-05-01",
-        "location": "Warsaw, Poland",
-        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/AbbottLogo.svg",
+        "id": "U3Vud2luZw==",
+        "companyName": "Sunwing",
+        "position": "Software Engineer",
+        "dateStart": "2024-09-16",
+        "dateEnd": "Current",
+        "location": "Toronto, ON",
+        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/SunwingLogo.png",
         "technologies": [
-            "Python",
-            "AWS",
-            "Terraform",
-            "Apache Spark"
+            "C#",
+            ".NET",
+            "Azure",
+            "SQL Server"
         ],
         "jobResponsibilities": [
-            "Supported Abbott's internal Big-Data/AI AWS-based platform. Specifically, was responsible for debugging issues related to infrastructure provisioning, deployment and exploitation.",
-            "Contributed to the data processing AWS Lambda functions and Apache Spark based ETL Glue Jobs",
-            "Developed AWS CloudFormation templates and Terraform configuration files for the AWS resources that were later utilized by the members of a Data Science team.",
-            "Developed and integrated a custom Bash script into CodePipeline workflow, which was responsible for automated build and deployment of the AWS Cloud Functions across the Abbott's AWS environment."
-        ]
-    },
-    {
-        "id": "RFhDIFRlY2hub2xvZ3k=",
-        "companyName": "DXC Technology",
-        "position": "Data Engineer",
-        "dateStart": "2023-01-16",
-        "dateEnd": "2023-05-01",
-        "location": "Warsaw, Poland",
-        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/DXCLogo.png",
-        "technologies": [
-            "Azure"
-        ],
-        "jobResponsibilities": [
-            "Developed and maintained Azure-based Data Pipelines for processing the stock/inventory data of a big FMCG Client. Pipelines were built by using Python, Azure Databricks, Apache Spark, Azure Datafactory and Synapse Analytics.",
-            "Developed C# (.NET) based Azure Functions as part of major pipeline workflows for performing an additional data enrichment operations.",
-            "Built CI/CD pipelines and automated tests for the Data Pipelines and Azure functions by making use of Azure DevOps Pipelines.",
-            "Created Terraform configrations for the deployment of the new Data Processing clusters and Database Instances on Azure."
-        ]
-    },
-    {
-        "id": "RGV2b3RlYW0gRyBDbG91ZA==",
-        "companyName": "Devoteam G Cloud",
-        "position": "Cloud Support Engineer",
-        "dateStart": "2022-05-01",
-        "dateEnd": "2023-01-16",
-        "location": "Warsaw, Poland",
-        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/DevoteamLogo.png",
-        "technologies": [
-            "Python",
-            "Terraform",
-            "Google Cloud"
-        ],
-        "jobResponsibilities": [
-            "Initiated and developed a solution on top of the Google's open source project - 'gcpdiag', that was automatically comparing clients' infrastructure deployed in the GCP against the most common bad practices that could cause major issues. Results were automatically returned to the Support Engineer assigned to the ticket, which significantly assisted him with the further resolvement of the issue occured.",
-            "Developed GCP Cloud Functions for automation of the manual routine tasks that were constantly performed in the Support Department.",
-            "Served as a first line Support Engineer for the Clients with the Google Cloud based Infrastructure"
+            "Served as an individual contributor and maintainer of the core .NET-based backend Sunwing interfaces such as User Profile and User Auth APIs.",
+            "Participated in the integration project with the Westjet Airlines. Specifically, was responsible for interconnection of the core backend Sunwing APIs with internal services of the Westjet.",
+            "Maintained and enhanced internal .NET-based Batch Jobs. Improvement: Introduced an automatic reprocessing logic that reduced the amount of issues that should be handled manually by 80%.",
+            "Continuosly collborated with the Technical Leaders, Product Managers and QA Engineers in order to ensure the maximum quality of the features delivered."
         ]
     },
     {
@@ -98,10 +61,71 @@ const testData = [
             "Apache Airflow"
         ],
         "jobResponsibilities": [
-            "Initiated, architected and developed Golang-based custom parsing solution for automatic modification of Terraform configuration files and exposed it to internal teams using a REST API. The API was consumed by the major P&G project 'AI Factory' for provisioning AI infrastructure in GCP.",
-            "Developed a solution for monitoring BigQuery processing slot consumption across the P&G GCP environment. The project utilized Python, BigQuery, GCP's Apache Airflow (Cloud Composer), and Looker Studio. The solution was later used by Cloud Support teams to identify performance issues and resolve them faster.",
+            "Initiated, architected and developed Golang-based custom parsing solution for automatic modification of Terraform configuration files and exposed it via a REST API. Solution reduced manual efforts, when creating GCP resources, by 80%.",
+            "Developed a solution in a form of Dashboard for monitoring BigQuery processing slot consumption across the P&G's GCP environment.The solution allowed Cloud Support Team to identify and resolve issues 50% faster.(Avg. resolution time was reduced from 1 hour to 30 minutes.",
             "Guided application teams on best practices for deploying solutions on P&G's GCP platform. Reviewed and adjusted architectures, created CI/CD pipelines (Jenkins, Cloud Build, GitHub Actions), and developed Terraform configurations.",
-            "Packaged GCP resources and reusable application architectures into Terraform modules for the later usage by application teams. Security and performance best practices were enforced in the modules delivered."
+            "Packaged GCP resources and reusable application architectures into Terraform modules for the later usage by application teams. Enforced security and performance best practices in the modules delivered."
+        ]
+    },
+    {
+        "id": "RFhDIFRlY2hub2xvZ3k=",
+        "companyName": "DXC Technology",
+        "position": "Data Engineer",
+        "dateStart": "2023-01-16",
+        "dateEnd": "2023-05-01",
+        "location": "Warsaw, Poland",
+        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/DXCLogo.png",
+        "technologies": [
+            "Python",
+            "Azure",
+            "SQL Server",
+            "Apache Spark"
+        ],
+        "jobResponsibilities": [
+            "Developed and maintained Azure-based Data Pipelines for processing the stock/inventory data of a big FMCG Client.",
+            "Developed C# (.NET) based Azure Functions as part of major pipeline workflows for performing an additional data enrichment operations.",
+            "Built CI/CD pipelines and automated tests for the Data Pipelines and Azure functions by making use of Azure DevOps Pipelines.",
+            "Created Terraform configurations for the deployment of the new Data Processing clusters and Database Instances on Azure."
+        ]
+    },
+    {
+        "id": "RGV2b3RlYW0gRyBDbG91ZA==",
+        "companyName": "Devoteam G Cloud",
+        "position": "Cloud Support Engineer",
+        "dateStart": "2022-05-01",
+        "dateEnd": "2023-01-16",
+        "location": "Warsaw, Poland",
+        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/DevoteamLogo.png",
+        "technologies": [
+            "Python",
+            "Terraform",
+            "Google Cloud"
+        ],
+        "jobResponsibilities": [
+            "Developed a custom service on top of the Google's open source project - 'gcpdiag', which was automatically analyzing customer's infrastructure against the most common GCP's bad practices that could cause major issues. Solution reduced the debugging time for simple/medium issues by ~80%.",
+            "Developed GCP Cloud Functions for automation of the manual routine tasks that were constantly performed in the Support Department.",
+            "Served as a first line Support Engineer for the Clients with the Google Cloud based Infrastructure"
+        ]
+    },
+    {
+        "id": "QWJib3R0",
+        "companyName": "Abbott",
+        "position": "Intern DevOps/Data Engineer",
+        "dateStart": "2021-12-01",
+        "dateEnd": "2022-05-01",
+        "location": "Warsaw, Poland",
+        "logoUrl": "https://raw.githubusercontent.com/mvksxm/personal-website/refs/heads/master/frontend/personal-website/public/images/logos/AbbottLogo.svg",
+        "technologies": [
+            "Python",
+            "AWS",
+            "Terraform",
+            "Apache Spark"
+        ],
+        "jobResponsibilities": [
+            "Supported Abbott's internal Big-Data/AI AWS-based platform. Specifically, was responsible for debugging issues related to infrastructure provisioning, deployment and exploitation.",
+            "Contributed to the data processing AWS Lambda functions and Apache Spark based ETL Glue Jobs",
+            "Developed AWS CloudFormation templates and Terraform configuration files for the AWS resources that were later utilized by the members of a Data Science team.",
+            "Developed and integrated a custom Bash script into CodePipeline workflow, which was responsible for automated build and deployment of the AWS Cloud Functions across the Abbott's AWS environment. Solution completely eliminated the need for manual deployment."
         ]
     }
 ]
@@ -127,7 +151,6 @@ const arrangeResp = (jobResp: string[]) => {
 }
 
 const generateWorkFrames = (elements: WorkExperienceType[]): React.ReactElement<StringDivProps>[] => {
-
     elements.sort((a, b) => {
         let dsa = new Date(a["dateStart"])
         let dsb = new Date(b["dateStart"])
@@ -198,7 +221,6 @@ const generateWorkFrames = (elements: WorkExperienceType[]): React.ReactElement<
 }
 
 const Work = () => {
-    
     const [workExperience, setWorkExperience] = useState<WorkExperienceType[]>([])
     
     useEffect(() => {
@@ -210,11 +232,14 @@ const Work = () => {
                 }
             }
         ).then(resp => {
-            if (resp.statusText === "OK" && resp.data !== null) {
+            if (resp.status === 200 && resp.data) {
+                console.log(resp.data)
                 setWorkExperience(resp.data)
-            } else if (resp.statusText !== "OK") {
+            } else if (resp.status !== 200) {
+                console.error(`Endpoint - ${BACKEND_URL} returned the following error status code - ${resp.status}!`)
                 throw new Error(`Endpoint - ${BACKEND_URL} returned the following error status code - ${resp.status}!`)
             } else if (resp.data === null) {
+                console.error(`Empty data was returned from the following url - ${BACKEND_URL}!`)
                 throw new Error(`Empty data was returned from the following url - ${BACKEND_URL}!`)
             }
         }).catch((err) => {
@@ -223,12 +248,11 @@ const Work = () => {
 
     }, [])
     
-    
     return (
         <div className="work-container">
             <h1 className="work-header">Work Experience</h1>
             <div className="work-content">
-                <Carousel elements={generateWorkFrames(testData)}/>
+                {workExperience.length > 0 ? <Carousel elements={generateWorkFrames(workExperience)}/> : <div/>}
                 {/* {generateWorkFrames(testData)[1]} */}
                 {/* <div className="work-frame">
                     <div className="work-frame-header">
--- frontend/personal-website/vite.config.ts
+++ frontend/personal-website/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as dotenv from 'dotenv'
+
+// Dotenv config
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true
+  },
   plugins: [react()],
+  define: {
+    "process.env.BACKEND_URL": JSON.stringify(process.env.BACKEND_URL),
+    "process.env.WORK_EXPERIENCE_COLLECTION": JSON.stringify(process.env.WORK_EXPERIENCE_COLLECTION),
+  }
 })
